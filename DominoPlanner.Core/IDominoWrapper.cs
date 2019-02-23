a440836a0524228a4f94dc7e5c4d45eb4e1f670a
jhofinger <jonathan.hofinger@gmx.de>
2019-02-23 18:51:57 +0100

    Added a factory method for IDominoWrappers

--- DominoPlanner.Core/IDominoWrapper.cs
+++ DominoPlanner.Core/IDominoWrapper.cs
@@ -32,6 +32,27 @@ namespace DominoPlanner.Core
             if (parent != null) // rootnode
                 parent.children.Add(this);
         }
+        public IDominoWrapper CreateNodeFromPath(DominoAssembly futureParent, string path)
+        {
+            var parentPath = Workspace.Instance.openedFiles.Find(x => x.Item2 == futureParent).Item1;
+            var relPath = Workspace.MakeRelativePath(parentPath, path);
+            var deserialized = Workspace.Load<IWorkspaceLoadable>(path);
+            switch (deserialized)
+            {
+                case FieldParameters p:
+                    return new FieldNode(relPath, futureParent);
+                case StructureParameters p:
+                    return new StructureNode(relPath, futureParent);
+                case SpiralParameters p:
+                    return new SpiralNode(relPath, futureParent);
+                case CircleParameters p:
+                    return new CircleNode(relPath, futureParent);
+                case DominoAssembly p:
+                    return new AssemblyNode(relPath, futureParent);
+                default:
+                    throw new ArgumentException("Path is not loadable");
+            }
+        }
     }
     [ProtoContract]
     public class DominoAssembly : IWorkspaceLoadable
@@ -98,6 +119,7 @@ namespace DominoPlanner.Core
         {
             this.relativePath = relativePath;
         }
+        
     }
     [ProtoContract(SkipConstructor = true)]
     public class FieldNode : DocumentNode
--- DominoPlanner.Core/Workspace.cs
+++ DominoPlanner.Core/Workspace.cs
@@ -177,5 +177,25 @@ namespace DominoPlanner.Core
             if (result.Count() == 0) return null;
             return result.First().Item2;
         }
+        public static String MakeRelativePath(String fromPath, String toPath)
+        {
+            if (String.IsNullOrEmpty(fromPath)) throw new ArgumentNullException("fromPath");
+            if (String.IsNullOrEmpty(toPath)) throw new ArgumentNullException("toPath");
+
+            Uri fromUri = new Uri(fromPath);
+            Uri toUri = new Uri(toPath);
+
+            if (fromUri.Scheme != toUri.Scheme) { return toPath; } // path can't be made relative.
+
+            Uri relativeUri = fromUri.MakeRelativeUri(toUri);
+            String relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+            if (toUri.Scheme.Equals("file", StringComparison.InvariantCultureIgnoreCase))
+            {
+                relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+            }
+
+            return relativePath;
+        }
     }
 }
