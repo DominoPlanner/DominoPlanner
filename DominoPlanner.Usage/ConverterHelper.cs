2163101a276eba3e689c10cb97251a8dcb81276f
Jonathan Hofinger <jonathan.hofinger@gmx.de>
2020-10-22 11:15:02 +0200

    add tooltip to tools in editproject

--- DominoPlanner.Usage/ConverterHelper.cs
+++ DominoPlanner.Usage/ConverterHelper.cs
@@ -264,7 +264,7 @@ namespace DominoPlanner.Usage
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return ((Enum)value).HasFlag((Enum)parameter);
+            return ((Enum)value).Equals((Enum)parameter);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
