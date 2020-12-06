0de0f061202e21af6eed0d56db2629a29814f9c2
Jonathan Hofinger <jonathan.hofinger@gmx.de>
2020-12-06 11:42:28 +0100

    retarget Core as netstandard2.0

--- DominoPlanner.Core/Workspace.cs
+++ DominoPlanner.Core/Workspace.cs
@@ -324,7 +324,20 @@ namespace DominoPlanner.Core
         {
             if (string.IsNullOrEmpty(fromPath)) return toPath;
             if (string.IsNullOrEmpty(toPath)) throw new ArgumentNullException("toPath");
-            return Path.GetRelativePath(Path.GetDirectoryName(fromPath), toPath);
+            Uri fromUri = new Uri(fromPath);
+            Uri toUri = new Uri(toPath);
+
+            if (fromUri.Scheme != toUri.Scheme) { return toPath; } // path can't be made relative.
+
+            Uri relativeUri = fromUri.MakeRelativeUri(toUri);
+            String relativePath = Uri.UnescapeDataString(relativeUri.ToString());
+
+            if (toUri.Scheme.Equals("file", StringComparison.InvariantCultureIgnoreCase))
+            {
+                relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+            }
+
+            return relativePath;
         }
         
     }
