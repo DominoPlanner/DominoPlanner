794a54d04fe3e8d91eee8ea5002875a6a556c353
Jonathan Hofinger <jonathan.hofinger@gmx.de>
2021-02-28 17:48:44 +0100

    Make color list buttons shorter so they fit the screen

--- DominoPlanner.Usage/ImageButton.cs
+++ DominoPlanner.Usage/ImageButton.cs
@@ -1,6 +1,9 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data.Converters;
 using Avalonia.Media;
+using System;
+using System.Globalization;
 
 namespace DominoPlanner.Usage
 {
@@ -14,5 +17,19 @@ namespace DominoPlanner.Usage
         public static readonly StyledProperty<IImage> ImageProperty = AvaloniaProperty.Register<ImageButton, IImage>(nameof(Image));
 
         public ImageButton() { }
+    }
+    public class IsNotEmptyStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return false;
+            return !(value is string s && string.IsNullOrEmpty(s));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
