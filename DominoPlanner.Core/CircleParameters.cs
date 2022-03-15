d6f8b0bbd91d79977f2c533bc8e39fb89b50b842
Johannes Linnenbrink <johannes.linnenbrink@t-online.de>
2022-03-15 20:07:19 +0100

    make circle diameter variable

--- DominoPlanner.Core/CircleParameters.cs
+++ DominoPlanner.Core/CircleParameters.cs
@@ -90,6 +90,25 @@ namespace DominoPlanner.Core
                 }
             }
         }
+
+        private bool _TryUsePerfectInnerCircle = false;
+        [ProtoMember(5)]
+        public bool TryUsePerfectInnerCircle
+        {
+            get
+            {
+                return _TryUsePerfectInnerCircle;
+            }
+            set
+            {
+                if (_TryUsePerfectInnerCircle != value)
+                {
+                    _TryUsePerfectInnerCircle = value;
+                    shapesValid = false;
+                }
+            }
+        }
+
         private Random r;
 
         public CircleParameters(string filepath, string imagepath, int circles,
@@ -117,33 +136,19 @@ namespace DominoPlanner.Core
             DominoLength = 24;
             NormalDistance = 8;
             TangentialDistance = 8;
-            this.StartDiameter = 4 * DominoLength;
+            StartDiameter = 4 * DominoLength;
         }
         private CircleParameters() : base() { r = new Random(); }
         public override void RegenerateShapes()
         {
             PathDomino[][] dominos = new PathDomino[Circles][];
-            Parallel.For(0,  Circles,  new ParallelOptions() { MaxDegreeOfParallelism = -1 },
-            (circlecount) =>
+
+            if (TryUsePerfectInnerCircle)
             {
-                int diameter = StartDiameter + circlecount * (2 * DominoWidth + 2 * NormalDistance);
-                double domino_angle = Math.Asin((double)DominoLength / diameter) * 2;
-                double distance_angle = Math.Asin((double)TangentialDistance / diameter) * 2;
-                int current_domino_count = (int)Math.Floor(2 * Math.PI / ((double)domino_angle + distance_angle));
-                current_domino_count = (int)Math.Floor((double)current_domino_count / _force_divisibilty) * _force_divisibilty;
-                // equally space the distance between all dominoes
-                distance_angle = (2 * Math.PI - (domino_angle * current_domino_count)) / current_domino_count;
-                // calculate dominoes
-                double angle = (double)AngleShiftFactor*circlecount;
-                dominos[circlecount] = new PathDomino[current_domino_count];
-                for (int i = 0; i < current_domino_count; i++)
-                {
-                    PathDomino d = GenerateDomino(diameter, angle);
-                    angle += domino_angle + distance_angle;
-                    d.position = new ProtocolDefinition() { x = i, y = circlecount };
-                    dominos[circlecount][i] = d;
-                }
-            });
+                _RegenerateShapes_CalcInnerTwoCircles(dominos);
+            }
+            _RegenerateShapes_CalcOuterCircles(dominos);
+            
             IDominoShape[] dominoes = dominos.SelectMany(x => x).ToArray();
             DominoRectangle[] containers = dominoes.AsParallel().Select(x => x.GetContainer()).ToArray();
             
@@ -159,6 +164,90 @@ namespace DominoPlanner.Core
             Last = new DominoTransfer(dominoes, colors);
             shapesValid = true;
         }
+
+        private void _RegenerateShapes_CalcOuterCircles(PathDomino[][] dominos)
+        {
+            Parallel.For(TryUsePerfectInnerCircle ? 2 : 0, Circles, new ParallelOptions() { MaxDegreeOfParallelism = -1 },
+            (circlecount) =>
+            {
+                int diameter = _CalcDiameter(circlecount);
+                double domino_angle = _CalcAngle(DominoLength, diameter);
+                double distance_angle = _CalcAngle(TangentialDistance, diameter);
+                int current_domino_count = calcDominoAmount(domino_angle, distance_angle);
+                // equally space the distance between all dominoes
+                distance_angle = (2 * Math.PI - (domino_angle * current_domino_count)) / current_domino_count;
+                _RegenerateShapes_CalcDominoPathes(dominos, circlecount, diameter, domino_angle, distance_angle, current_domino_count, (double)AngleShiftFactor);
+            });
+        }
+
+        private void _RegenerateShapes_CalcInnerTwoCircles(PathDomino[][] dominos)
+        {
+            double firstStoneAngle = 0;
+            int firstCircleStoneAmount = 1;
+
+            for (int circlecount = 0; circlecount < 2; circlecount++)
+            {
+                int diameter = _CalcDiameter(circlecount);
+                double domino_angle = _CalcAngle(DominoLength, diameter);
+
+                double currentTangentialDistance = TangentialDistance;
+                double distance_angle = _CalcAngle(TangentialDistance, diameter);
+                int current_domino_count = calcDominoAmount(domino_angle, distance_angle);
+
+                while (circlecount == 1 && current_domino_count != firstCircleStoneAmount * 2 && currentTangentialDistance > 2)
+                {
+                    currentTangentialDistance--;
+                    distance_angle = _CalcAngle(currentTangentialDistance, diameter);
+                    current_domino_count = calcDominoAmount(domino_angle, distance_angle);
+                }
+
+                firstCircleStoneAmount = current_domino_count;
+                // equally space the distance between all dominoes
+                distance_angle = (2 * Math.PI - (domino_angle * current_domino_count)) / current_domino_count;
+
+                double usedAngleShiftFactor = (double)AngleShiftFactor;
+                if (circlecount == 1)
+                {
+                    usedAngleShiftFactor = (firstStoneAngle - (domino_angle + distance_angle)) / 2;
+                }
+                // calculate dominoes
+                double angle = (double)usedAngleShiftFactor * circlecount;
+                angle += domino_angle + distance_angle;
+                if (circlecount == 0) firstStoneAngle = angle;
+
+                _RegenerateShapes_CalcDominoPathes(dominos, circlecount, diameter, domino_angle, distance_angle, current_domino_count, usedAngleShiftFactor);
+            }
+        }
+
+        private void _RegenerateShapes_CalcDominoPathes(PathDomino[][] dominos, int circlecount, int diameter, double domino_angle, double distance_angle, int current_domino_count, double angleShiftFactor)
+        {
+            double angle = angleShiftFactor * circlecount;
+            dominos[circlecount] = new PathDomino[current_domino_count];
+            for (int i = 0; i < current_domino_count; i++)
+            {
+                PathDomino d = GenerateDomino(diameter, angle);
+                angle += domino_angle + distance_angle;
+                d.position = new ProtocolDefinition() { x = i, y = circlecount };
+                dominos[circlecount][i] = d;
+            }
+        }
+
+        private int _CalcDiameter(int circleCount)
+        {
+            return StartDiameter + circleCount * (2 * DominoWidth + 2 * NormalDistance);
+        }
+
+        private double _CalcAngle(double value, int diameter)
+        {
+            return Math.Asin(value / diameter) * 2;
+        }
+
+        private int calcDominoAmount(double domino_angle, double distance_angle)
+        {
+            double current_domino_count = Math.Floor(2 * Math.PI / (domino_angle + distance_angle));
+            return (int)Math.Floor(current_domino_count / _force_divisibilty) * _force_divisibilty;
+        }
+
         private PathDomino GenerateDomino(int diameter, double angle)
         {
             double x1 = diameter / 2d * Math.Cos(angle);
